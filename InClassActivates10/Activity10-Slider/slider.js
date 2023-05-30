72335ad2f4b3a2dba9bfed8a53fe4462a80d95fc
esrefkayaa <esrefkaya6806@gmail.com>
2023-05-30 17:23:44 +0300

    finished

--- InClassActivates10/Activity10-Slider/slider.js
+++ InClassActivates10/Activity10-Slider/slider.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 	$("#slider").bxSlider({
 		auto: true,
+		pager: true,
 		randomStart: true,
+		captions: true,
 		minSlides: 1,
 		maxSlides: 1,
 		moveSlides: 1,
 		slideWidth: 250,
 		slideMargin: 10,
 		pause: 3000,
-		pager: true,
 		pagerCustom: '#pager',
-		captions: true,
-		pagerType: 'short',
+		pagerType: 'short'
 	});
 });
