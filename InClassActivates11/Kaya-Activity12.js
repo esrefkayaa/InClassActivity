f55d59795d8cc84dd144362ea112f6de0d645079
esrefkayaa <esrefkaya6806@gmail.com>
2023-06-05 22:29:38 +0300

    finished

--- InClassActivates11/Kaya-Activity12.js
+++ InClassActivates11/Kaya-Activity12.js
@@ -1,3 +1,27 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
+
+    // Event handler for navigation links
+    $("#nav_list li a").click(function (e) {
+
+        e.preventDefault();
+
+        //Load json data
+        $.getJSON("json_files/" + $(this).attr("title") + ".json", function (data) {
+
+            //I clear previous content
+            $("main").empty();
+
+            //selecting first speaker
+            var speaker = data.speakers[0];
+
+            // I added new content to the selected main element.
+            $("main")
+                .append(
+                    $("<h1>").text(speaker.title),
+                    $("<img>").attr("src", speaker.image),
+                    $("<h2>").html(speaker.month + "<br>" + speaker.speaker),
+                    $("<p>").text(speaker.text)
+                );
+        })
+    });
 }); // end ready
